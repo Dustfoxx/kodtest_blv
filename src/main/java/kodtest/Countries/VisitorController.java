34554e30095342a1b6e27c1f121a222b52c7e1ea
Fox Hamrén <fox.hamren@gmail.com>
2026-05-31 15:49:23 +0200

    self reference links included

--- src/main/java/kodtest/Countries/VisitorController.java
+++ src/main/java/kodtest/Countries/VisitorController.java
@@ -1,6 +1,7 @@
 package kodtest.Countries;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,31 +10,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class VisitorController {
     private final VisitorRepository repository;
 
+    private EntityModel<Visitor> createEntityModelFromVisitor(Visitor visitor){
+        return EntityModel.of(visitor,
+          linkTo(methodOn(VisitorController.class).getFromId(visitor.getId())).withSelfRel(),
+          linkTo(methodOn(VisitorController.class).all()).withRel("employees"));
+    }
+
     VisitorController(VisitorRepository repository){
         this.repository = repository;
     }
 
-    @GetMapping("/visitor")
-    List<Visitor> all(){
-        return repository.findAll();
+    @GetMapping("/visitors")
+    CollectionModel<EntityModel<Visitor>> all(){
+        List<EntityModel<Visitor>> visitors = repository.findAll().stream()
+        .map(visitor -> createEntityModelFromVisitor(visitor)).collect(Collectors.toList());
+
+        return CollectionModel.of(visitors, linkTo(methodOn(VisitorController.class).all()).withSelfRel());
     }
 
-    @PostMapping("/visitor")
+    @PostMapping("/visitors")
     Visitor newVisitor(@RequestBody Visitor newVisitor){
         return repository.save(newVisitor);
     }
 
-    @GetMapping("/visitor/{id}")
-    Visitor getFromId(@PathVariable Long id){
-        return repository.findById(id).orElseThrow(() -> new VisitorNotFoundException(id));
+    @GetMapping("/visitors/{id}")
+    EntityModel<Visitor> getFromId(@PathVariable Long id){
+        Visitor  visitor = repository.findById(id).orElseThrow(() -> new VisitorNotFoundException(id));
+        return createEntityModelFromVisitor(visitor);
     }
 
-    @PutMapping("/visitor/{id}")
+    @PutMapping("/visitors/{id}")
     Visitor replaceVisitor(@RequestBody Visitor newVisitor, @PathVariable Long id){
         return repository.findById(id)
         .map(visitor -> {
@@ -44,7 +58,7 @@ public class VisitorController {
         });
     }
 
-    @DeleteMapping("/visitor/{id}")
+    @DeleteMapping("/visitors/{id}")
     void deleteVisitor(@PathVariable Long id) {
         repository.deleteById(id);
     }
